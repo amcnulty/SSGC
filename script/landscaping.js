088733f99a3e14a8280fdaa4152715831a8ddc84
amcnulty <amcnulty88@swbell.net>
2017-11-03 22:28:17 -0700

    Landscaping page
    Styled the get a bid box on the landscaping page.
    Added the logic to make the get a bid box stay on the screen while user scrolls.

--- script/landscaping.js
+++ script/landscaping.js
@@ -2,4 +2,24 @@ if (window.attachEvent) {window.attachEvent('onload', load);}
 else if (window.addEventListener) {window.addEventListener('load', load, false);}
 else {document.addEventListener('load', load, false);}
 function load() {
+    function scroll() {
+        if (window.pageYOffset > 300) {
+            $("header").attr("class", "attached");
+        }
+        else {
+            $("header").attr("class", "");
+        }
+        // if ($('#bidBox').offset().top + 650 > $(footer).offset().top - 40) {
+            // console.log("Stop Scrolling");
+        // }
+        // else if (window.pageYOffset > $("#mainContent").offset().top - 50) {
+            // }
+            if (window.pageYOffset > $("#mainContent").offset().top - 50 && window.pageYOffset < $(footer).offset().top - 800) {
+                $("#bidBox").css({ top: (window.pageYOffset - $("#mainContent").offset().top - 50) + 150 + "px"});
+        }
+    }
+
+    console.log($("#bidBox").offset().top);
+
+    window.addEventListener("scroll", scroll, false);
 }
